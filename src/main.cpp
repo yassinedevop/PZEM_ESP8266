ad5a130b7ec30ae6c9f78cd2088951f556fb86c2
yassinedevop <yassineseghr15@hotmail.com>
2023-05-15 22:24:23 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,111 +1,29 @@
-#include <SoftwareSerial.h>
-#include <ModbusMaster.h>
-
-#include <ESP8266WiFi.h>
-#include <FirebaseESP8266.h>
-
-
-
-
-// Replace with your network credentials
-const char* ssid = "your_SSID";
-const char* password = "your_PASSWORD";
-
-// Replace with your Firebase project's configuration
-#define FIREBASE_HOST "your_project_ID.firebaseio.com"
-#define FIREBASE_AUTH "your_service_account_key"
-#define WIFI_SSID "your_wifi_ssid"
-#define WIFI_PASSWORD "your_wifi_password"
-
-FirebaseData firebaseData;
-
-
-// Create a SoftwareSerial object to communicate with the PZEM-004t
-SoftwareSerial pzemSerial(5, 4);
-
-// Create a ModbusMaster object to handle the Modbus communication
-ModbusMaster node;
-
-
-
-
-void loop() {
-  // Send data to Firestore
-  Firebase.setFloat(firebaseData, "collection/document/field", 123.45);
-
-  if (Firebase.failed()) {
-    Serial.println("Failed to send data to Firestore");
-    Serial.println(Firebase.error());
-  } else {
-    Serial.println("Sent data to Firestore");
-  }
-
-  // Wait for 5 seconds
-  delay(5000);
-}
-
+#include "main.h"
 
 void setup()
 {
-  // Set up the serial communication
-  Serial.begin(9600);
-  pzemSerial.begin(9600);
 
-  // Set up the Modbus communication
-  node.begin(1, pzemSerial); // id = 1, Serial = pzemSerial
+    Serial.begin(9600);
 
-  // Connect to WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-
-  Serial.println("Connected to WiFi");
-
-  // Initialize Firebase
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  Serial.println("Initialized Firebase");
-
-
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
 }
 
 void loop()
 {
-  // Read the voltage from the PZEM-004t
-  uint16_t voltage;
-  int8_t result = node.readInputRegisters(0x0000, 1);
-  if (result == node.ku8MBSuccess)
-  {
-    voltage = node.getResponseBuffer(0);
-    Serial.print("Voltage: ");
-    Serial.println(voltage);
-  }
-  else
-  {
-    Serial.print("Error reading voltage: ");
-    Serial.println(result);
-  }
-
-  // Read the current from the PZEM-004t
-  uint16_t currentMSB, currentLSB;
-  result = node.readInputRegisters(0x0001, 1);
-  if (result == node.ku8MBSuccess)
-  {
-    currentLSB = node.getResponseBuffer(0);
-    Serial.print("CurrentMSB: ");
-    Serial.println(currentMSB);
-    Serial.print("CurrentLSB: ");
-    Serial.println(currentLSB);
-  }
-  else
-  {
-    Serial.print("Error reading current: ");
-    Serial.println(result);
-  }
-  // now we will send this data to firebase
-
-
-
+    PZEMUpdate();
+    readSwitchStateFromFirebase();
+    if (voltage != NAN && current != NAN && power != NAN && energy != NAN && frequency != NAN && pf != NAN)
+        if (switchState != NAN)
+            sendDataToFirebase();
+    delay(1000);
 }
---
+++ src/main.h
@@ -0,0 +1,143 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PZEM004Tv30.h>
+#include <ArduinoJson.h>
+#define WIFI_SSID "Hp"
+#define WIFI_PASSWORD "12345678"
+
+PZEM004Tv30 pzem(5, 4); // Software Serial pin 5 (RX) & 4 (TX)
+
+float voltage, current, power, energy, frequency, pf;
+int switchState = NAN;
+const String url = "https://pzem004t-esp12f-default-rtdb.europe-west1.firebasedatabase.app/pzem.json";
+
+void PZEMUpdate()
+{
+    voltage = pzem.voltage();
+    if (voltage != NAN)
+    {
+        Serial.print("Voltage: ");
+        Serial.print(voltage);
+        Serial.println("V");
+    }
+    else
+    {
+        Serial.println("Error reading voltage");
+    }
+    current = pzem.current();
+    if (current != NAN)
+    {
+        Serial.print("Current: ");
+        Serial.print(current);
+        Serial.println("A");
+    }
+    else
+    {
+        Serial.println("Error reading current");
+    }
+    power = pzem.power();
+    if (current != NAN)
+    {
+        Serial.print("Power: ");
+        Serial.print(power);
+        Serial.println("W");
+    }
+    else
+    {
+        Serial.println("Error reading power");
+    }
+    energy = pzem.energy();
+    if (current != NAN)
+    {
+        Serial.print("Energy: ");
+        Serial.print(energy, 3);
+        Serial.println("kWh");
+    }
+    else
+    {
+        Serial.println("Error reading energy");
+    }
+    frequency = pzem.frequency();
+    if (current != NAN)
+    {
+        Serial.print("Frequency: ");
+        Serial.print(frequency, 1);
+        Serial.println("Hz");
+    }
+    else
+    {
+        Serial.println("Error reading frequency");
+    }
+    pf = pzem.pf();
+    if (current != NAN)
+    {
+        Serial.print("PF: ");
+        Serial.println(pf);
+    }
+    else
+    {
+        Serial.println("Error reading power factor");
+    }
+    Serial.println();
+    delay(2000);
+}
+void readSwitchStateFromFirebase()
+{
+    WiFiClientSecure client;
+    client.setInsecure();
+    HTTPClient http;
+
+    http.begin(client, url);
+    http.addHeader("Content-Type", "application/json");
+    int httpResponseCode = http.GET();
+    if (httpResponseCode > 0)
+    {
+        String response = http.getString();
+        // parse json the response
+        StaticJsonDocument<200> doc;
+        auto err = deserializeJson(doc, response);
+        if (err)
+        {
+            Serial.print("deserializeJson() failed with code ");
+            Serial.println(err.c_str());
+            return;
+        }
+        else
+        {
+            Serial.println("JSON parsed successfully!");
+            digitalWrite(15, doc["switchState"]);
+        }
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+    }
+    else
+    {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+}
+
+// now we will send the readings to firebase
+void sendDataToFirebase()
+{
+    WiFiClientSecure client;
+    client.setInsecure();
+    HTTPClient http;
+
+    http.begin(client, url);
+    http.addHeader("Content-Type", "application/json");
+    String data = "{\"voltage\": " + String(voltage) + ", \"current\": " + String(current) + ", \"power\": " + String(power) + ", \"energy\": " + String(energy) + ", \"frequency\": " + String(frequency) + ", \"pf\": " + String(pf) + ", \"switchState\": " + String(switchState) + "}";
+    int httpResponseCode = http.PUT(data);
+    if (httpResponseCode > 0)
+    {
+        String response = http.getString();
+        Serial.println(httpResponseCode);
+        Serial.println(response);
+    }
+    else
+    {
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+    http.end();
+}
